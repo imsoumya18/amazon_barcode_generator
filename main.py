72f9b24164b7a479008502a5128c9bc78180db80
imsoumya18 <soumyadeep184@gmail.com>
2021-03-30 11:57:48 +0530

    png to jpg

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from barcode import Code39
 from barcode.writer import ImageWriter
+from PIL import Image
+import os
 
 number = '123456789026'
 
 code = Code39(number, writer=ImageWriter())
 
 code.save('new')
+
+im = Image.open(r'new.png')
+im.save(r'new.jpg')
+
+os.remove('new.png')
